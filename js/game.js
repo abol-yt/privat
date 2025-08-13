408e22baae8ffe2f94fe2ff95f79098e03bddb74
abol-yt <soltan.amir.soltan.1383@gmail.com>
2025-08-13 16:04:35 +0200

    Update game.js

--- js/game.js
+++ js/game.js
@@ -9432,7 +9432,7 @@ window.addEventListener("load", function () {
       function (p650) {
         v760 = p650;
         $.ajax({
-          url: "https://25yt551.github.io/worm2/api/skin.php",
+          url: "https://wormup.in/store/indexx.php",
           method: "GET",
           dataType: "json",
           success: function (p651) {
@@ -9901,6 +9901,7 @@ this.injectCSS = addCSS;
 this.injectCSS();
 
 console.log("CSS injected!");
+
 
 
 
